71cc37e18d849e11b02f52a3c50cd1ba77c3c7a3
Primo <paoloalinab@gmail.com>
2025-12-28 15:11:51 +0800

    Fix form submit validation and form reset

--- src/index.js
+++ src/index.js
@@ -1,39 +1,78 @@
 import "./styles.css"
+import { format, compareAsc } from "date-fns";
 import { showDialog, closeDialog } from "./event.js"
 
-function makeToDoList() {
-    // possible additions: date and priority 
+function toDoListConsole() {
     const toDoListStorage = [
-        {"School": ["Read chapter 10 - physiology of the brain"]},
-        {"School": ["Apply for the programming varsity tryouts"]}
+        {project: "school", title: "Read chapter 10 - physiology of the brain", description: "Must finish urgently", date: "Jun 9", priority: "high"},
+        {project: "school", title: "Apply for the programming varsity tryouts", description: "Review or fail", date: "Jun 9", priority: "high"},
     ]
     
-    const checkToDoList = () => toDoListStorage.forEach((project) => {
-        console.log(project["School"])
+    const checkToDoList = () => toDoListStorage.forEach((list) => {
+        console.log(list)
     })
 
+    const addToDoList = (project, title, description, date, priority) => {
+        toDoListStorage.push({ project, title, description, date, priority})
+    }
+
     return {
-        toDoListStorage,
-        checkToDoList
+        checkToDoList,
+        addToDoList,
     }
 }
 
-function displayToDoList(view) {
-    
+const currentList = new toDoListConsole()
+currentList.checkToDoList()
+currentList.addToDoList("school", "Attending a computer vision workshop", "Learn a thing or two about AI", "June 22", "Medium")
+
+function extractDataFromForm(currentList) {
+    const listTitle = document.getElementById("list-title")
+    const listDescription = document.getElementById("list-description")
+    const listProject = document.getElementById("project")
+    const listDeadline = document.getElementById("deadline")
+    const listPriority = document.querySelector('input[name="priority"]:checked')
+
+    const listTitleText = listTitle.value
+    const listDescriptionText = listDescription.value
+    const listProjectText = listProject.value
+    const listDeadlineText = listDeadline.value
+    const listPriorityText = listPriority.value
+
+    console.log(listTitleText)
+    console.log(listDescriptionText)
+    console.log(listProjectText)
+    console.log(listDeadlineText)
+    console.log(listPriorityText)
 }
 
-// buttons
+
+// Just buttons
 const addTaskButton = document.getElementById("add-task-button")
 const addTaskDialog = document.getElementById("add-task-dialog")
 const addProjectButton = document.getElementById("add-project-button")
 const addProjectDialog = document.getElementById("add-project-dialog")
-const submitDialogProjectButton = document.getElementById("submit-project")
-const submitDialogTaskButton = document.getElementById("submit-task")
 const cancelDialogProjectButton = document.getElementById("close-project")
 const cancelDialogTaskButton = document.getElementById("close-task")
+const addTaskForm = document.getElementById("add-task-form")
+const addProjectForm = document.getElementById("add-project-form")
 
 addTaskButton.addEventListener("click", () => showDialog(addTaskDialog))
 addProjectButton.addEventListener("click", () => showDialog(addProjectDialog))
+
+addTaskDialog.addEventListener("submit", (event) => {
+    event.preventDefault()
+    extractDataFromForm(currentList)
+    addTaskForm.reset()
+    closeDialog(addTaskDialog)
+})
+
+addProjectDialog.addEventListener("submit", (event) => {
+    event.preventDefault()
+    addProjectForm.reset()
+    closeDialog(addProjectDialog)
+})
+
 cancelDialogTaskButton.addEventListener("click", (event) => {
     event.preventDefault()
     closeDialog(addTaskDialog)
@@ -43,6 +82,3 @@ cancelDialogProjectButton.addEventListener("click", (event) => {
     event.preventDefault()
     closeDialog(addProjectDialog)
 })
-
-
-makeToDoList()
