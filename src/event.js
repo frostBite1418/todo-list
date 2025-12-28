c38cd5aefedbe826af8f0aa1186bc8da0fdbeb18
Primo <paoloalinab@gmail.com>
2025-12-28 12:39:10 +0800

    Make working dialog modals taking information to make a new todo list with accompanied features such as opening and closing

---
+++ src/event.js
@@ -0,0 +1,9 @@
+function showDialog(dialog){
+    dialog.showModal()
+}
+
+function closeDialog(dialog){
+    dialog.close()
+}
+
+export { showDialog, closeDialog }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import "./styles.css"
+import { showDialog, closeDialog } from "./event.js"
+
+function makeToDoList() {
+    // possible additions: date and priority 
+    const toDoListStorage = [
+        {"School": ["Read chapter 10 - physiology of the brain"]},
+        {"School": ["Apply for the programming varsity tryouts"]}
+    ]
+    
+    const checkToDoList = () => toDoListStorage.forEach((project) => {
+        console.log(project["School"])
+    })
+
+    return {
+        toDoListStorage,
+        checkToDoList
+    }
+}
+
+function displayToDoList(view) {
+    
+}
+
+// buttons
+const addTaskButton = document.getElementById("add-task-button")
+const addTaskDialog = document.getElementById("add-task-dialog")
+const addProjectButton = document.getElementById("add-project-button")
+const addProjectDialog = document.getElementById("add-project-dialog")
+const submitDialogProjectButton = document.getElementById("submit-project")
+const submitDialogTaskButton = document.getElementById("submit-task")
+const cancelDialogProjectButton = document.getElementById("close-project")
+const cancelDialogTaskButton = document.getElementById("close-task")
+
+addTaskButton.addEventListener("click", () => showDialog(addTaskDialog))
+addProjectButton.addEventListener("click", () => showDialog(addProjectDialog))
+cancelDialogTaskButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    closeDialog(addTaskDialog)
+})
+
+cancelDialogProjectButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    closeDialog(addProjectDialog)
+})
+
+
+makeToDoList()
